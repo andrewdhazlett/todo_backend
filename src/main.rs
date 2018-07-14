17e337ff8449902b82c168b626d4fb3e3cf8bfd0
andrewdhazlett <andrew@hazlett.me>
2018-07-13 21:46:34 -0500

    task(core): create a server and App with no assets

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+extern crate actix;
+extern crate actix_web;
+
+use actix::{System};
+use actix_web::{App, server};
+
 fn main() {
-    println!("Hello, world!");
+    let sys = System::new("todo_backend");
+    server::new(|| App::new())
+        .bind("localhost:3000")
+        .unwrap()
+        .start();
+    sys.run();
 }
