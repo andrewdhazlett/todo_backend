8ed620df985595866cb28decd8f4b238e909b91d
andrewdhazlett <andrew@hazlett.me>
2018-07-13 21:59:29 -0500

    feat(core): implement hello-world route

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 extern crate actix;
 extern crate actix_web;
 
-use actix::{System};
-use actix_web::{App, server};
+use actix::System;
+use actix_web::{http, server, App, HttpRequest, HttpResponse};
 
 fn main() {
     let sys = System::new("todo_backend");
-    server::new(|| App::new())
-        .bind("localhost:3000")
+    server::new(|| {
+        App::new().route(
+            "/",
+            http::Method::GET,
+            |_: HttpRequest| -> HttpResponse {
+                HttpResponse::Ok().body("Hello, world!")
+            },
+        )
+    }).bind("localhost:3000")
         .unwrap()
         .start();
     sys.run();
