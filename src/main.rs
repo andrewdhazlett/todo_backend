9fd7dcdd352e7e590459174e4b72ac3146322784
andrewdhazlett <andrew@hazlett.me>
2018-07-13 22:28:16 -0500

    feat(core): implement Path extractor

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,26 @@
 extern crate actix;
 extern crate actix_web;
+#[macro_use]
+extern crate serde_derive;
 
 use actix::System;
-use actix_web::{http, server, App, HttpRequest, HttpResponse};
+use actix_web::{http, server, App, Path, Result};
+
+#[derive(Deserialize)]
+struct GreetingParams {
+    name: String,
+}
 
 fn main() {
     let sys = System::new("todo_backend");
     server::new(|| {
-        App::new().route(
-            "/",
-            http::Method::GET,
-            |_: HttpRequest| -> HttpResponse {
-                HttpResponse::Ok().body("Hello, world!")
-            },
-        )
+        App::new().resource("/{name}", |req| {
+            req.method(http::Method::GET).with(
+                |params: Path<GreetingParams>| -> Result<String> {
+                    Ok(format!("Hello, {}", params.name))
+                },
+            )
+        })
     }).bind("localhost:3000")
         .unwrap()
         .start();
