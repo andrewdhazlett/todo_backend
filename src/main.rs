63d03de31fc33a14b1934abd61a251681e1ed59a
andrewdhazlett <andrew@hazlett.me>
2018-07-14 10:19:20 -0500

    feat(core): introduce mutable state

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,12 @@ extern crate actix_web;
 extern crate serde_derive;
 
 use actix::System;
-use actix_web::{http, server, App, Path, Result};
+use actix_web::{http, server, App, Path, State};
+use std::cell::RefCell;
+
+struct TodoState {
+    name: RefCell<String>,
+}
 
 #[derive(Deserialize)]
 struct GreetingParams {
@@ -14,13 +19,28 @@ struct GreetingParams {
 fn main() {
     let sys = System::new("todo_backend");
     server::new(|| {
-        App::new().resource("/{name}", |req| {
-            req.method(http::Method::GET).with(
-                |params: Path<GreetingParams>| -> Result<String> {
-                    Ok(format!("Hello, {}", params.name))
-                },
-            )
-        })
+        let state = TodoState {
+            name: RefCell::from("Fred".to_string()),
+        };
+        App::with_state(state)
+            .resource("/", |req| {
+                req.method(http::Method::GET)
+                    .with(|state: State<TodoState>| {
+                        let name = state.name.try_borrow().unwrap();
+                        format!("Hello, {}!", &name)
+                    })
+            })
+            .resource("/{name}", |req| {
+                req.method(http::Method::PUT).with(
+                    |(state, params): (
+                        State<TodoState>,
+                        Path<GreetingParams>,
+                    )| {
+                        state.name.replace(params.name.clone());
+                        "success"
+                    },
+                );
+            })
     }).bind("localhost:3000")
         .unwrap()
         .start();
