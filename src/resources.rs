3004680978f1ee3aa14715e785f31a7c1d99d5af
Andrew Hazlett <andrew@drawn.com>
2018-07-16 12:58:41 -0500

    feature(todos): POST todo mutates state

--- src/resources.rs
+++ src/resources.rs
@@ -6,14 +6,16 @@ pub fn get_todos(state: State<TodoState>) -> Json<Vec<Todo>> {
     Json(state.todos.borrow().clone())
 }
 
-pub fn create_todo(body: Json<NewTodo>) -> Json<Todo> {
-    Json(Todo {
+pub fn create_todo((state, body): (State<TodoState>, Json<NewTodo>)) -> Json<Todo> {
+    let todo = Todo {
         id: Uuid::nil().to_string(),
         title: body.title.clone(),
         order: 0,
         completed: false,
         url: "".to_string(),
-    })
+    };
+    state.todos.borrow_mut().push(todo.clone());
+    Json(todo)
 }
 
 pub fn delete_todos(state: State<TodoState>) -> Json<Vec<Todo>> {
